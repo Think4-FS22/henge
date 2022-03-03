a5a1fa00da5a394a15c31b08e3139019d6b4ba4c
chrischne <chrischne@gmail.com>
2022-03-03 21:01:29 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,14 +8,14 @@ let bounds = {
   bottom: 47.024504,
 };
 
-// let mousepos;
-// let center;
-// let mousedir;
-// let mousehdg;
+let center = {
+  lat: 47.050167,
+  lon: 8.309307,
+};
 
-let sunriseHdg;
-let sunriseDir;
-let centerCoords;
+// let sunriseHdg;
+let sunDir;
+// let centerCoords;
 let counter = 0;
 function preload() {
   geodata = loadJSON("lucerne-roads.geojson");
@@ -24,78 +24,58 @@ function preload() {
 let slider;
 
 let startDate = new Date(2022, 0, 1);
-console.log('startDate', startDate);
 let endDate = new Date(2022, 11, 31);
-console.log('endDate', endDate);
-let timeScale = d3.scaleLinear().domain([0, 365]).range([startDate, endDate]);
 
-let currentDate = timeScale(0);
-console.log('currentDate', currentDate)
+let timeScale = d3.scaleLinear().domain([0, 365]).range([startDate, endDate]);
 
 function setup() {
   createCanvas(900, 650);
   angleMode(DEGREES);
-  centerCoords = createVector(8.309307, 47.050167);
-  slider = createSlider(0, 365, 0)
+  //  centerCoords = createVector(8.309307, 47.050167);
+  slider = createSlider(0, 365, 0, 1);
   slider.position(10, 10);
-  slider.changed(function () {
-
-    currentDate = timeScale(slider.value());
-    console.log('currentDate', currentDate)
-    calcSunDir();
-    redraw();
-  })
+  slider.changed(updateDate);
   currentDate = timeScale(slider.value());
-  calcSunDir();
-
-
+  // calcSunDir();
 
   roads = geodata.features;
   console.log(roads);
 
-frameRate(3);
+  frameRate(3);
 
-//  noLoop();
+  sunDir = calcSunVector(currentDate, center.lon, center.lat);
+
+  noLoop();
 }
 
 function draw() {
   background(0);
 
-  counter++;
-  if(counter>365){
-    counter = 0;
-  }
-  currentDate = timeScale(counter);
-  calcSunDir();
-
-  let cx = map(centerCoords.x, bounds.left, bounds.right, 0, width);
-  let cy = map(centerCoords.y, bounds.bottom, bounds.top, height, 0);
-  let center = createVector(cx, cy);
+  let cx = map(center.lon, bounds.left, bounds.right, 0, width);
+  let cy = map(center.lat, bounds.bottom, bounds.top, height, 0);
+  let centerPoint = createVector(cx, cy);
   fill(255);
   ellipse(cx, cy, 10, 10);
-  let p2 = p5.Vector.add(center, sunriseDir);
+  let sunpos = p5.Vector.add(centerPoint, sunDir);
 
-  ellipse(p2.x, p2.y, 10, 10);
+  fill(255, 255, 0);
+  ellipse(sunpos.x, sunpos.y, 10, 10);
   stroke(255);
-  line(cx, cy, p2.x, p2.y);
-
+  line(cx, cy, sunpos.x, sunpos.y);
 
   noStroke();
   fill(255);
   text(currentDate, 100, 100);
 
-
-
   for (let i = 0; i < roads.length; i++) {
     let coordinates = roads[i].geometry.coordinates;
 
-    stroke(0)
+    stroke(0);
     for (let j = 0; j < coordinates.length - 1; j++) {
       let lon1 = coordinates[j][0];
       let lat1 = coordinates[j][1];
       let lon2 = coordinates[j + 1][0];
       let lat2 = coordinates[j + 1][1];
-
 
       let x1 = map(lon1, bounds.left, bounds.right, 0, width);
       let y1 = map(lat1, bounds.bottom, bounds.top, height, 0);
@@ -110,21 +90,19 @@ function draw() {
 
       // let hdg = dir.heading()+90;
 
-      let theta = dir.angleBetween(sunriseDir);
-      let theta2 = dir2.angleBetween(sunriseDir);
+      let theta = dir.angleBetween(sunDir);
+      let theta2 = dir2.angleBetween(sunDir);
       let eps = 5;
       //  console.log('theta',theta,theta2)
 
       if (abs(theta) < eps || abs(theta2) < eps) {
         stroke(255, 255, 0, 100);
         strokeWeight(2);
-
-      }
-      else {
+      } else {
         stroke(255, 30);
-        strokeWeight(1)
-      } line(x1, y1, x2, y2);
-
+        strokeWeight(1);
+      }
+      line(x1, y1, x2, y2);
 
       //   if(hdg>-80 && hdg<mousehdg+10){
       //     stroke(255,255,0,100);
@@ -134,11 +112,7 @@ function draw() {
       //     stroke(255,30);
       //     strokeWeight(1)
       //   }
-
-
-
     }
-
   }
 
   // stroke(255,0,0);
@@ -158,21 +132,57 @@ function keyTyped() {
 //   redraw();
 // }
 
-
 function calcSunDir() {
   var times = SunCalc.getTimes(currentDate, centerCoords.y, centerCoords.x);
-  var sunrisePos = SunCalc.getPosition(times.sunrise, centerCoords.y, centerCoords.x);
-  console.log('times', times);
-  console.log('sunrisePos', sunrisePos);
+  var sunrisePos = SunCalc.getPosition(
+    times.sunrise,
+    centerCoords.y,
+    centerCoords.x
+  );
+  console.log("times", times);
+  console.log("sunrisePos", sunrisePos);
 
-  var sunriseAzimuth = sunrisePos.azimuth * 180 / Math.PI;
-  sunriseHdg = degrees(sunrisePos.azimuth)
+  var sunriseAzimuth = (sunrisePos.azimuth * 180) / Math.PI;
+  sunriseHdg = degrees(sunrisePos.azimuth);
   sunriseDir = createVector(100, 0);
-  console.log('initial headinng', sunriseDir.heading())
+  console.log("initial headinng", sunriseDir.heading());
   sunriseDir.rotate(sunriseHdg - 90);
-  console.log('new headinng', sunriseDir.heading())
+  console.log("new headinng", sunriseDir.heading());
   // sunriseDir.rotate(-180)
 
-  console.log('sunriseAzimuth', sunriseAzimuth, degrees(sunrisePos.azimuth), sunriseHdg);
+  console.log(
+    "sunriseAzimuth",
+    sunriseAzimuth,
+    degrees(sunrisePos.azimuth),
+    sunriseHdg
+  );
+}
 
+function updateDate() {
+  currentDate = timeScale(slider.value());
+  console.log("currentDate", currentDate);
+  // calcSunDir();
+  sunDir = calcSunVector(currentDate, 8.309307, 47.050167);
+
+  redraw();
+}
+
+function calcSunVector(date, lon, lat) {
+  console.log("calcSunVector");
+  var times = SunCalc.getTimes(date, lat, lon);
+  var sunPos = SunCalc.getPosition(times.sunrise, lat, lon);
+  console.log("times", times);
+  console.log("sunPos", sunPos);
+  var sunAzimuth = (sunPos.azimuth * 180) / Math.PI;
+
+  // var sunriseAzimuth = (sunrisePos.azimuth * 180) / Math.PI;
+  let sunHdg = degrees(sunPos.azimuth);
+  let sunDir = createVector(100, 0);
+  console.log("initial headinng", sunDir.heading());
+  sunDir.rotate(sunHdg - 90);
+  console.log("new headinng", sunDir.heading());
+  // sunriseDir.rotate(-180)
+
+  console.log("sunriseAzimuth", sunAzimuth, degrees(sunPos.azimuth), sunHdg);
+  return sunDir;
 }
